efae22eb12e06649fce10c42cec8e83346c43f62
lxxyeon <lxxyeon@gmail.com>
2023-11-06 20:15:59 +0900

    Project init

---
+++ 311TEN022/311TEN022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  311TEN022
+//
+//  Created by leeyeon2 on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
