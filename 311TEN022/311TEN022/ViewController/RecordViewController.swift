10feec2dd0e17ee8ab5926ee870b3de8a7ad44cd
lxxyeon <lxxyeon@gmail.com>
2023-11-07 23:07:48 +0900

    [Add] ActionSheet 를 통한 카메라, 라이브러리 선택 기능 추가

--- 311TEN022/311TEN022/ViewController/RecordViewController.swift
+++ 311TEN022/311TEN022/ViewController/RecordViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class RecordViewController: UIViewController {
-    
+class RecordViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var Picker = UIImagePickerController()
     @IBOutlet weak var objectImageView: UIImageView!
     var tagButtonArray = [UIButton]()
     var buyList = ["메ㅗㄹ","ㅇㅇㅇ"]
@@ -24,12 +24,54 @@ class RecordViewController: UIViewController {
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
     {
         let tappedImage = tapGestureRecognizer.view as! UIImageView
-
-        // Your actionp
-        print("click")
-        
+        showActionSheet()
     }
     
+    /// 이미지 변경 버튼 클릭시 생성되는 action sheet
+    func showActionSheet() {
+        
+        // 액션 시트 초기화
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // UIAlertAction 설정
+        // handler : 액션 발생시 호출
+        let actionAlbum = UIAlertAction(title: "앨범에서 선택할래요", style: .default, handler: {(alert:UIAlertAction!) -> Void in
+            self.openGallery()
+        })
+        let actionCamera = UIAlertAction(title: "사진 찍을래요", style: .default, handler: {(alert:UIAlertAction!) -> Void in
+            self.openCamera()
+        })
+
+        let actionCancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        actionSheet.addAction(actionCamera)
+        actionSheet.addAction(actionAlbum)
+        actionSheet.addAction(actionCancel)
+        
+        self.present(actionSheet, animated: true)
+    }
+    /// actionsheet1. 카메라 촬영
+    func openCamera()
+    {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+        {
+            self.Picker.sourceType = UIImagePickerController.SourceType.camera;
+            self .present(self.Picker, animated: true, completion: nil)
+            self.Picker.allowsEditing = false
+            self.Picker.delegate = self
+        }
+    }
+    
+    /// actionsheet2. 앨범에서 가져오기
+    func openGallery()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.savedPhotosAlbum){
+            self.Picker.sourceType = UIImagePickerController.SourceType.savedPhotosAlbum;
+            self.Picker.allowsEditing = false
+            self.Picker.delegate = self
+            self.present(self.Picker, animated: true, completion: nil)
+        }
+    }
     
     /// 태그뷰 초기화
     private func initTagView() {
