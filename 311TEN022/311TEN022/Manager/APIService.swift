80fe324b25cb580c1dd9c244877badfacb23bafa
lxxyeon <lxxyeon@gmail.com>
2023-11-11 00:00:23 +0900

    [Add] Network API 구현

--- 311TEN022/311TEN022/Manager/APIService.swift
+++ 311TEN022/311TEN022/Manager/APIService.swift
@@ -9,9 +9,108 @@ import Foundation
 import Alamofire
 import UIKit
 
+struct Config {
+    static let baseURL = "http://118.67.129.168:8080"
+    static let headers: HTTPHeaders = ["Content-Type" : "application/json"]
+    //    static let headers: HTTPHeaders = ["Accept" : "application/json, application/javascript, text/javascript, text/json",
+    //                                       "Content-Type" : "application/json; charset=UTF-8"]
+}
+
+
 final class APIService {
     static let shared = APIService()
+    var headers: HTTPHeaders = Config.headers
+    let memberId = UserDefaults.standard.integer(forKey: "memberId")
     
+    //MARK: - Goal
+    func getgoal(param: Parameters,
+                 completion: @escaping (Int) -> Void) {
+        let url = Config.baseURL+"/goal/\(4)"
+        AF.request(url,
+                   method: .get,
+                   headers: headers).responseString { response in
+            switch response.result{
+            case .success(_):
+                do{
+                    let dataString = String(data: response.data!, encoding: .utf8)
+                    if let jsonData = dataString!.data(using: .utf8) {
+                        if let jsonDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            let userId = jsonDict["mission"] as! Int
+                            completion(1)
+                        }
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+            default:
+                completion(0)
+            }
+            
+        }
+    }
+    
+    func goalSave(param: Parameters,
+                  completion: @escaping (Int) -> Void) {
+        
+        let url = Config.baseURL+"/goal/\(4)"
+        AF.request(url,
+                   method: .post,
+                   parameters: param,
+                   encoding: JSONEncoding.default,
+                   headers: headers).responseString { response in
+            switch response.result{
+                //200인 경우 성공
+            case .success(_):
+                do{
+                    let dataString = String(data: response.data!, encoding: .utf8)
+                    if let jsonData = dataString!.data(using: .utf8) {
+                        if let jsonDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            //                            let userId = jsonDict["msg"] as! Int
+                            completion(1)
+                        }
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+            default:
+                completion(0)
+            }
+            
+        }
+    }
+    
+    
+    
+    //로그인
+    //http://118.67.129.168:8080/member/login
+    func signIn(param: Parameters,
+                completion: @escaping (Int) -> Void) {
+        let url = Config.baseURL+"/member/login"
+        AF.request(url,
+                   method: .post,
+                   parameters: param,
+                   encoding: JSONEncoding.default,
+                   headers: headers).responseString { response in
+            switch response.result{
+                //200인 경우 성공
+            case .success(_):
+                do{
+                    let dataString = String(data: response.data!, encoding: .utf8)
+                    if let jsonData = dataString!.data(using: .utf8) {
+                        if let jsonDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            let userId = jsonDict["memberId"] as! Int
+                            completion(userId)
+                        }
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+            default:
+                completion(0)
+            }
+            
+        }
+    }
     
     // MARK: - 파일 업로드 API
     func fileUpload(imageData: UIImage,
