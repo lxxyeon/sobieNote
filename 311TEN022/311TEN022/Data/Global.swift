81f43c42668cfff6d81177894ec2a8698519475b
lxxyeon <lxxyeon@gmail.com>
2023-12-16 23:44:02 +0900

    [Add] 재사용을 위한 CalendarView custom(.xib)로 생성

---
+++ 311TEN022/311TEN022/Data/Global.swift
@@ -0,0 +1,8 @@
+//
+//  Global.swift
+//  311TEN022
+//
+//  Created by leeyeon2 on 12/16/23.
+//
+
+import Foundation
---
+++ 311TEN022/311TEN022/View/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  311TEN022
+//
+//  Created by leeyeon2 on 12/16/23.
+//
+
+import UIKit
+
+class CalendarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
