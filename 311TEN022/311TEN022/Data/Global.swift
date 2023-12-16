352c5e9f8425bc963f312f8089e16e277f5becba
lxxyeon <lxxyeon@gmail.com>
2023-12-16 23:52:37 +0900

    [Fix] 전역변수로 현재 날짜, 선택 날짜 저장

--- 311TEN022/311TEN022/Data/Global.swift
+++ 311TEN022/311TEN022/Data/Global.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Global {
+    static let shared = Global()
+    
+    // 현재 날짜(년, 월)
+    var selectedYear: String!
+    var selectedMonth: String!
+    var currentYear: String!
+    var currentMonth: String!
+    
+    private init() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        self.selectedYear = formatter.string(from: Date())
+        self.currentYear = formatter.string(from: Date())
+        formatter.dateFormat = "MM"
+        self.selectedMonth = formatter.string(from: Date())
+        self.currentMonth = formatter.string(from: Date())
+    }
+}
--- 311TEN022/311TEN022/View/CalendarView.swift
+++ 311TEN022/311TEN022/View/CalendarView.swift
@@ -8,13 +8,110 @@
 import UIKit
 
 class CalendarView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var yearStackView: UIStackView!
+    @IBOutlet weak var yearTitleLabel: UILabel!{
+        didSet{
+            yearTitleLabel.text = Global.shared.selectedYear + "년"
+        }
     }
-    */
+    
+    let xibName = "CalendarView"
+    var selectedMonth: String = Global.shared.selectedMonth
+    var selectedYear: String = Global.shared.selectedYear
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    private func commonInit(){
+        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+        yearStackView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapYearStackView(sender:)))
+        yearStackView.addGestureRecognizer(tap)
+        
+        let btnStackView = ButtonStackView()
+        self.addSubview(btnStackView)
+    }
+    
+    @objc func didTapYearStackView (sender: UITapGestureRecognizer) {
+        // 연도 변경을 위한 선택
+//        yearTitleLabel.text =
+        // 선택시 월버튼 연버튼으로
+        print("Stack was tapped!")
+    }
 }
+
+
+class ButtonStackView: UIStackView {
+    var buttonList = Array(1...12).map{"\($0)월"}
+
+    
+//    init() {
+//        super.init(
+//        self.buttonList = Array(1...12).map{"\($0)월"}
+//    }
+    
+//    required init(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    func buttonStackView() -> UIStackView {
+        return self.createButtonStack(stakList: self.buttonList)
+    }
+
+    func createButtonStack(stakList: [String]) -> UIStackView {
+        let buttonListStackView: UIStackView = {
+            let buttonListStackView = UIStackView()
+            buttonListStackView.alignment = .fill
+            buttonListStackView.distribution = .fillEqually
+            buttonListStackView.backgroundColor = .white
+            buttonListStackView.spacing = 10
+            buttonListStackView.axis = .vertical
+            return buttonListStackView
+        }()
+        
+        for i in 1...3 {
+            let buttonStackView: UIStackView = {
+                let buttonStackView = UIStackView()
+                buttonStackView.alignment = .fill
+                buttonStackView.distribution = .fillEqually
+                buttonStackView.backgroundColor = .white
+                buttonStackView.spacing = 10
+                buttonStackView.axis = .horizontal
+               
+                for i in 1...4 {
+                    let monthButton = UIButton()
+                    monthButton.setTitle("월", for: .normal)
+                    buttonStackView.addArrangedSubview(monthButton)
+                }
+
+                return buttonStackView
+            }()
+            
+            buttonListStackView.addArrangedSubview(buttonStackView)
+        }
+
+        
+        return buttonListStackView
+    }
+}
+
+//class ButtonView: ButtonStackView {
+//    override func createButtonStack(stakList: [UIView]) -> UIStackView {
+//        let buttonView = super.createButtonStack(stakList: stakList)
+//        
+//        buttonView.alignment = .center
+//        buttonView.distribution = .fillProportionally
+//        buttonView.axis = .vertical
+//        return buttonView
+//    }
+//}
