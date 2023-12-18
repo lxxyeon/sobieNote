e4715fb4a395b0f6df305e1d79e16afd6bfe53c8
lxxyeon <lxxyeon@gmail.com>
2023-12-18 20:49:19 +0900

    메인 화면 addsubview 캘린더뷰 추가

--- 311TEN022/311TEN022/ViewController/TabViewController/HomeTabViewController.swift
+++ 311TEN022/311TEN022/ViewController/TabViewController/HomeTabViewController.swift
@@ -8,7 +8,72 @@
 import UIKit
 import Alamofire
 
+// TAB1. 메인 화면
 class HomeTabViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var TitleStackView: UIStackView!
+    //
+
+    @IBOutlet weak var titleLabel: UILabel!{
+        didSet{
+            titleLabel.text = "\(Global.shared.selectedMonth!)월 소비기록"
+        }
+    }
+    
+    let calendarView = CalendarView()
+    var calendarIsHidden: Bool = true
+    
+    // 타이틀스택 클릭시 calendar 보여주는 action
+    @objc func didTapStackView (sender: UITapGestureRecognizer) {
+        if calendarIsHidden {
+            calendarView.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(calendarView)
+            
+            
+            NSLayoutConstraint.activate([
+                calendarView.topAnchor.constraint(equalTo: TitleStackView.topAnchor, constant: TitleStackView.frame.height),
+                calendarView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+                calendarView.heightAnchor.constraint(equalToConstant: self.view.frame.height),
+                calendarView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+            ])
+
+            calendarIsHidden = false
+        }else{
+            calendarView.removeFromSuperview()
+            calendarIsHidden = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataParsing()
+
+        TitleStackView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapStackView(sender:)))
+        TitleStackView.addGestureRecognizer(tap)
+        
+  
+        
+        
+        self.goalTextFiled.delegate = self
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        //        guard let collectionView = collectionView, let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        //
+        //        flowLayout.minimumInteritemSpacing = margin
+        //        flowLayout.minimumLineSpacing = margin
+        //        flowLayout.sectionInset = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+        
+    }
+    
+    
+    
     let margin: CGFloat = 0
     let textViewPlaceHolder = "목표를 적어주세요."
     func textViewDidEndEditing(_ textView: UITextView) {
@@ -24,10 +89,12 @@ class HomeTabViewController: UIViewController, UITextViewDelegate {
             goalTextFiled.textColor =  UIColor(hexCode: "FFF9CA")
         }
     }
-//    let textViewPlaceHolder2 = "목표를 적어주세요!"
+    //    let textViewPlaceHolder2 = "목표를 적어주세요!"
     @IBOutlet weak var goalTextFiled: UITextView!
+    
+    // 목표 전송
     @IBAction func sendGoal(_ sender: Any) {
-        print("목표 전송")
+        
         let mission = goalTextFiled.text
         let parameter: Parameters = [
             "mission": mission
@@ -35,20 +102,23 @@ class HomeTabViewController: UIViewController, UITextViewDelegate {
         
         UserDefaults.standard.setValue(mission, forKey: "mission")
         
-        APIService.shared.goalSave(param: parameter, completion: {_ in
-            var alert = UIAlertController()
-            alert = UIAlertController(title:"목표가 저장됐어요!",
-                                      message: "",
-                                      preferredStyle: UIAlertController.Style.alert)
-            self.present(alert,animated: true,completion: nil)
-            let buttonLabel = UIAlertAction(title: "확인", style: .default, handler: {_ in
-                self.dismiss(animated:true, completion: nil)
-            })
-            alert.addAction(buttonLabel)
+        let request = APIRequest(method: .post,
+                                 path: "/goal",
+                                 param: parameter,
+                                 headers: APIConfig.authHeaders)
+        
+        APIService.shared.perform(memberId: UserInfo.memberId,
+                                  request: request,
+                                  completion: { (result) in
+            switch result {
+            case .success(let response):
+                print("성공")
+            case .failure:
+                print(APIError.networkFailed)
+            }
         })
     }
     
-
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var goalView: UIView!{
@@ -86,8 +156,29 @@ class HomeTabViewController: UIViewController, UITextViewDelegate {
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
+    
     var imgList = [String]()
+    
     func dataParsing() {
+        // 이미지 list 가져오기
+        let date = "/2023/12"
+        let request = APIRequest(method: .get,
+                                 path: "/image" + date,
+                                 param: nil,
+                                 headers: APIConfig.authHeaders)
+        
+        APIService.shared.perform(memberId: UserInfo.memberId,
+                                  request: request,
+                                  completion: { (result) in
+            switch result {
+            case .success:
+                
+                print(result)
+            case .failure:
+                print(APIError.networkFailed)
+            }
+        })
+        
         if let imageDict = UserDefaults.standard.object([[Int: String]].self, with: "imgDict"){
             ImgdataCount = imageDict.count
             for i in imageDict{
@@ -98,29 +189,10 @@ class HomeTabViewController: UIViewController, UITextViewDelegate {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.dataParsing()
-        self.goalTextFiled.delegate = self
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide(_:)),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow(_:)),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-//        guard let collectionView = collectionView, let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
-//
-//        flowLayout.minimumInteritemSpacing = margin
-//        flowLayout.minimumLineSpacing = margin
-//        flowLayout.sectionInset = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
-        
-    }
-    
     
 }
 
+// MARK: - CollectionView Handling
 // Cell data 관련
 extension HomeTabViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -132,18 +204,18 @@ extension HomeTabViewController: UICollectionViewDelegate, UICollectionViewDataS
         var documentsUrl: URL {
             return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         }
-
-        let fileName = imgList[ImgdataCount-indexPath.row-1]
         
-        let fileURL = documentsUrl.appendingPathComponent(fileName)
-        do {
-            let imageData = try Data(contentsOf: fileURL)
-            cell.cellImage.image = UIImage(data: imageData)
-            cell.cellImage.contentMode = .scaleAspectFill
-        } catch {
-            print("Error loading image : \(error)")
-        }
-
+//        let fileName = imgList[ImgdataCount-indexPath.row-1]
+//        
+//        let fileURL = documentsUrl.appendingPathComponent(fileName)
+//        do {
+//            let imageData = try Data(contentsOf: fileURL)
+//            cell.cellImage.image = UIImage(data: imageData)
+//            cell.cellImage.contentMode = .scaleAspectFill
+//        } catch {
+//            print("Error loading image : \(error)")
+//        }
+        
         return cell
     }
 }
