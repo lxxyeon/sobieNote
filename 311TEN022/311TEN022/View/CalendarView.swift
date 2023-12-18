2f1030c0f680a0917ca4f25dae3637508c0874db
lxxyeon <lxxyeon@gmail.com>
2023-12-18 20:48:21 +0900

    [Add] 캘린더 커스텀 뷰 화면 개발

--- 311TEN022/311TEN022/View/CalendarView.swift
+++ 311TEN022/311TEN022/View/CalendarView.swift
@@ -7,111 +7,169 @@
 
 import UIKit
 
+// custom 캘린더 화면
 class CalendarView: UIView {
     
-    @IBOutlet weak var yearStackView: UIStackView!
-    @IBOutlet weak var yearTitleLabel: UILabel!{
-        didSet{
-            yearTitleLabel.text = Global.shared.selectedYear + "년"
-        }
-    }
-    
-    let xibName = "CalendarView"
+    // 선택시 변경될 날짜 변수
     var selectedMonth: String = Global.shared.selectedMonth
     var selectedYear: String = Global.shared.selectedYear
+    
+    // 연도스택뷰 선택 유무
+    var yearStackIsClicked: Bool = false
 
+    var buttonStackView = UIStackView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.commonInit()
+        self.setUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.commonInit()
     }
-    
-    private func commonInit(){
+
+    private func setUI(){
+        let xibName = "CalendarView"
         let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
-        yearStackView.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapYearStackView(sender:)))
-        yearStackView.addGestureRecognizer(tap)
         
-        let btnStackView = ButtonStackView()
-        self.addSubview(btnStackView)
+        // 버튼 title String, init : month
+//        var buttonTitleList = yearStackIsClicked ? Array(1...12).map{"\($0)년"} : Array(1...12).map{"\($0)월"}
+        buttonStackView = makebuttonStackView(buttonTitles: Array(1...12).map{"\($0)" + "월"})
+        //
+//        buttonStackView = makebuttonStackView(buttonTitles: Array(Int(Global.shared.currentYear
+//                                                                     )!-11...Int(Global.shared.currentYear
+//                                                                                )!).sorted(by: <).map{"\($0)" + "년"})
+        
+        baseView.addSubview(buttonStackView)
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.isLayoutMarginsRelativeArrangement = true
+        NSLayoutConstraint.activate(
+            [
+                buttonStackView.leadingAnchor.constraint(equalTo: yearStackView.leadingAnchor),
+                buttonStackView.trailingAnchor.constraint(equalTo: baseView.trailingAnchor, constant: -32),
+                buttonStackView.topAnchor.constraint(equalTo: yearStackView.bottomAnchor, constant: 10),
+                buttonStackView.bottomAnchor.constraint(equalTo: baseView.bottomAnchor, constant: -20)
+            ])
     }
     
-    @objc func didTapYearStackView (sender: UITapGestureRecognizer) {
-        // 연도 변경을 위한 선택
-//        yearTitleLabel.text =
-        // 선택시 월버튼 연버튼으로
-        print("Stack was tapped!")
-    }
-}
-
-
-class ButtonStackView: UIStackView {
-    var buttonList = Array(1...12).map{"\($0)월"}
-
-    
-//    init() {
-//        super.init(
-//        self.buttonList = Array(1...12).map{"\($0)월"}
-//    }
-    
-//    required init(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
-    func buttonStackView() -> UIStackView {
-        return self.createButtonStack(stakList: self.buttonList)
-    }
-
-    func createButtonStack(stakList: [String]) -> UIStackView {
-        let buttonListStackView: UIStackView = {
-            let buttonListStackView = UIStackView()
-            buttonListStackView.alignment = .fill
-            buttonListStackView.distribution = .fillEqually
-            buttonListStackView.backgroundColor = .white
-            buttonListStackView.spacing = 10
-            buttonListStackView.axis = .vertical
-            return buttonListStackView
+    private func makebuttonStackView(buttonTitles: [String]) -> UIStackView{
+        let verticalStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.alignment = .fill
+            stackView.distribution = .fillEqually
+            stackView.spacing = 10
+            return stackView
         }()
-        
-        for i in 1...3 {
-            let buttonStackView: UIStackView = {
-                let buttonStackView = UIStackView()
-                buttonStackView.alignment = .fill
-                buttonStackView.distribution = .fillEqually
-                buttonStackView.backgroundColor = .white
-                buttonStackView.spacing = 10
-                buttonStackView.axis = .horizontal
-               
-                for i in 1...4 {
-                    let monthButton = UIButton()
-                    monthButton.setTitle("월", for: .normal)
-                    buttonStackView.addArrangedSubview(monthButton)
-                }
-
-                return buttonStackView
+        var monthPlus = 0
+        for _ in 1...3 {
+            let horizontalStackView: UIStackView = {
+                let stackView = UIStackView()
+                stackView.axis = .horizontal
+                stackView.alignment = .fill
+                stackView.distribution = .fillEqually
+                stackView.spacing = 10
+                return stackView
             }()
             
-            buttonListStackView.addArrangedSubview(buttonStackView)
+            for i in 1...4{
+                let aButton: UIButton = {
+                    let button = UIButton(type: .custom)
+                    //월일 때
+                    let buttonTitle = buttonTitles[monthPlus]
+                    
+                    //년도일 때
+                    button.setTitle(buttonTitle, for: .normal)
+                    
+                    button.titleLabel?.font = .systemFont(ofSize: 18)
+                    button.setTitleColor(.black, for: .normal)
+                    
+                    //현재 선택된 날짜
+                    if String(buttonTitle.dropLast()) == Global.shared.selectedMonth || String(buttonTitle.dropLast()) == Global.shared.selectedYear {
+                        button.setBackgroundColor(.systemGray2, for: .normal)
+                    }else{
+                        button.setBackgroundColor(.systemGray4, for: .normal)
+                    }
+                    
+                    button.clipsToBounds = true
+                    button.layer.cornerRadius = 10
+                    button.addTarget(self, action: #selector(returnButtonData(_:)), for: .touchUpInside)
+                    return button
+                }()
+                horizontalStackView.addArrangedSubview(aButton)
+                monthPlus += 1
+            }
+            verticalStackView.addArrangedSubview(horizontalStackView)
+           
         }
+        return verticalStackView
+    }
 
-        
-        return buttonListStackView
+    @IBOutlet weak var baseView: UIView!{
+        didSet{
+            baseView.layer.cornerRadius = 20
+            baseView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMaxXMaxYCorner, .layerMinXMaxYCorner)
+        }
+    }
+    
+    @IBOutlet weak var yearStackView: UIStackView!{
+        didSet{
+            yearStackView.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(didTapYearStackView(sender:)))
+            yearStackView.addGestureRecognizer(tap)
+        }
+    }
+    
+    @IBOutlet weak var yearTitleLabel: UILabel!{
+        didSet{
+            // 현재 연도
+            yearTitleLabel.text = Global.shared.currentYear + "년"
+        }
+    }
+    
+    // MARK: - Action Method
+    // 버튼 선택 날짜로 이동
+    @objc func returnButtonData(_ sender: UIButton) {
+        if let title = sender.titleLabel {
+            let selectedDate = String(title.text!.dropLast())
+            if selectedDate.count > 2{
+                Global.shared.selectedYear = selectedDate
+            }else{
+                Global.shared.selectedMonth = selectedDate
+            }
+            //
+            self.removeFromSuperview()
+            print(selectedDate)
+        }
+    }
+    
+    // 연도 변경을 위한 선택 - 추후 수정 필요
+    @objc func didTapYearStackView (sender: UITapGestureRecognizer) {
+//        if yearStackIsClicked {
+//            yearStackIsClicked = false
+//            buttonStackView.removeFromSuperview()
+//            buttonStackView = makebuttonStackView(buttonTitles: Array(1...12).map{"\($0)년"})
+//            baseView.addSubview(buttonStackView)
+//        }else{
+//            yearStackIsClicked = true
+//            buttonStackView = makebuttonStackView(buttonTitles: Array(1...12).map{"\($0)월"})
+//            buttonStackView.removeFromSuperview()
+//            baseView.addSubview(buttonStackView)
+//        }
     }
 }
 
-//class ButtonView: ButtonStackView {
-//    override func createButtonStack(stakList: [UIView]) -> UIStackView {
-//        let buttonView = super.createButtonStack(stakList: stakList)
-//        
-//        buttonView.alignment = .center
-//        buttonView.distribution = .fillProportionally
-//        buttonView.axis = .vertical
-//        return buttonView
-//    }
-//}
+extension UIButton {
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.setBackgroundImage(backgroundImage, for: state)
+    }
+}
