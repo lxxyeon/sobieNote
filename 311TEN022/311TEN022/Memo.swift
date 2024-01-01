0d8c985b094782d1c746d94f65b1009ef84e7402
lxxyeon <lxxyeon@gmail.com>
2024-01-01 17:46:01 +0900

    [Add] Reference file add

--- 311TEN022/311TEN022/Memo.swift
+++ 311TEN022/311TEN022/Memo.swift
@@ -1,8 +1,16 @@
-//
-//  Memo.swift
-//  311TEN022
-//
-//  Created by leeyeon2 on 12/2/23.
-//
+//userdefault
+// memberid
+// String(UserDefaults.standard.integer(forKey: "memberId"))
+// auth token
+// UserDefaults.standard.integer(forKey: "memberId")
 
-import Foundation
+//화면
+//1. 홈화면(HomeTabViewController)
+//2. 기록 게시몰 업로드 화면(BoardTabViewController)
+//3. 보고서 화면(ReportTabViewController)
+
+//report
+//1. 구매카테고리 report view - categories TagList1
+//2. 구매감정 report view - emotions TagList2
+//3. 구매요인 report view - factors TagList3
+//4. 구매만족도 report view - satisfactions TagList4
