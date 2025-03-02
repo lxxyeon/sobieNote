63ac8db42c1f13b3d23626d8a51675d951aa50c7
안이연 <lxxyeon@gmail.com>
2025-03-02 23:43:11 +0900

    Mock data 별도 관리

---
+++ 311TEN022/311TEN022/Common/Mock.swift
@@ -0,0 +1,114 @@
+//
+//  Mock.swift
+//  311TEN022
+//
+//  Created by leeyeon2 on 3/2/25.
+//
+
+struct Mock {
+    // 이미지 데이터
+    let MockImgList = ["IMG_1642", "IMG_1643", "IMG_1644", "IMG_1645", "IMG_1647", "IMG_1649",
+                       "IMG_1651", "IMG_1654" , "IMG_1655", "IMG_1656", "IMG_1657", "IMG_1658", "IMG_1656", "IMG_1657", "IMG_1658"]
+    // 보고서 데이터
+    let responseReport = ReportData(keyword: "식품",
+                                    value: 6)
+    let responseReport2 = ReportData(keyword: "패션",
+                                     value: 8)
+    let responseReport3 = ReportData(keyword: "디지털",
+                                     value: 2)
+    let responseReport4 = ReportData(keyword: "미용",
+                                     value: 5)
+    let responseReport5 = ReportData(keyword: "반려동물",
+                                     value: 2)
+    let responseReport6 = ReportData(keyword: "스포츠",
+                                     value: 7)
+    let responseReport7 = ReportData(keyword: "인테리어",
+                                     value: 1)
+    
+    let responseReport8 = ReportData(keyword: "도서",
+                                    value: 3)
+    let responseReport9 = ReportData(keyword: "자동차",
+                                     value: 11)
+    let responseReport10 = ReportData(keyword: "가전",
+                                     value: 5)
+    let responseReport11 = ReportData(keyword: "건강",
+                                     value: 17)
+    let responseReport12 = ReportData(keyword: "생활용품",
+                                     value: 20)
+    let responseReport13 = ReportData(keyword: "취미",
+                                     value: 4)
+    let responseReport14 = ReportData(keyword: "여행",
+                                     value: 1)
+    
+    
+    let emotiondata1 = ReportData(keyword: "행복한",
+                                     value: 7)
+
+    let emotiondata2 = ReportData(keyword: "뿌듯한",
+                                     value: 6)
+    let emotiondata3 = ReportData(keyword: "신기한",
+                                     value: 3)
+
+    let emotiondata5 = ReportData(keyword: "후회하는",
+                                     value: 1)
+    let emotiondata6 = ReportData(keyword: "설레는",
+                                     value: 2)
+    
+    
+    let satisfactions1 = ReportData(keyword: "100",
+                                     value: 6)
+    let satisfactions2 = ReportData(keyword: "80",
+                                     value: 8)
+    let satisfactions3 = ReportData(keyword: "60",
+                                     value: 5)
+    let satisfactions4 = ReportData(keyword: "40",
+                                     value: 1)
+    let satisfactions5 = ReportData(keyword: "20",
+                                     value: 2)
+    
+    let factor1 = ReportData(keyword: "환경보호",
+                                     value: 21)
+    let factor2 = ReportData(keyword: "가효율증가",
+                                     value: 21)
+    let factor3 = ReportData(keyword: "자기계발",
+                                     value: 8)
+    let factor4 = ReportData(keyword: "호기심충족",
+                                     value: 1)
+    let factor5 = ReportData(keyword: "몸건강",
+                                     value: 8)
+    let factor6 = ReportData(keyword: "테스트",
+                                     value: 1)
+//    self.categories.append(responseReport)
+//    self.categories.append(responseReport2)
+//    self.categories.append(responseReport3)
+//    self.categories.append(responseReport4)
+//    self.categories.append(responseReport5)
+//    self.categories.append(responseReport6)
+//    self.categories.append(responseReport7)
+//    self.categories.append(responseReport8)
+//    self.categories.append(responseReport9)
+//    self.categories.append(responseReport10)
+//    self.categories.append(responseReport11)
+//    self.categories.append(responseReport12)
+//    self.categories.append(responseReport13)
+//    self.categories.append(responseReport14)
+//    
+//    self.emotions.append(emotiondata1)
+//    self.emotions.append(emotiondata2)
+//    self.emotions.append(emotiondata3)
+//    self.emotions.append(emotiondata6)
+//    self.emotions.append(emotiondata5)
+//    
+//    self.factors.append(factor6)
+//    self.factors.append(factor1)
+//    self.factors.append(factor2)
+//    self.factors.append(factor3)
+//    self.factors.append(factor4)
+//    self.factors.append(factor5)
+//    
+//    self.satisfactions.append(satisfactions1)
+//    self.satisfactions.append(satisfactions2)
+//    self.satisfactions.append(satisfactions3)
+//    self.satisfactions.append(satisfactions4)
+//    self.satisfactions.append(satisfactions5)
+}
