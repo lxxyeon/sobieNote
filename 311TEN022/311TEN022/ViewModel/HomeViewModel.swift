8a36b8ab4fe5b0126f80b011c21d4af3e353d146
안이연 <lxxyeon@gmail.com>
2025-07-01 16:45:52 +0900

    HomeViewmodel 추가

--- 311TEN022/311TEN022/ViewModel/HomeViewModel.swift
+++ 311TEN022/311TEN022/ViewModel/HomeViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Alamofire
+
+final class HomeViewModel {
+    
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    private var isEndPage: Bool = true
+    
+    struct Output {
+        let images: Observable<[BoardImage]>
+    }
+
+}
